a5b0e80257179b81ad7a250cff38df9e8c0b6df3
Saur03 <chawlasaurabh97@gmail.com>
2022-11-03 11:39:29 +0530

    stack example

---
+++ stack.py
@@ -0,0 +1,14 @@
+'''
+Create a stack
+Stack uses LIFO
+'''
+
+stack = []
+stack.append("Welcome")
+stack.append("to")
+stack.append("great learning")
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack)
