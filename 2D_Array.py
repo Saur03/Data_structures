0d926abb224fa2c6a99bc5a7f39dd7b7aa975336
Saur03 <chawlasaurabh97@gmail.com>
2023-07-12 18:23:52 +0530

    add coments and description after understanding

--- 2D_Array.py
+++ 2D_Array.py
@@ -1,3 +1,8 @@
+''''
+- 2-D Array
+- Declare number of rows and columns in a matrix
+
+'''
 r = int(input("Enter the number of rows: "))
 c = int(input("Enter the number of columns: "))
 twod_arr = [[0 for col in range(c)] for row in range(r)]
@@ -5,4 +10,5 @@ twod_arr = [[0 for col in range(c)] for row in range(r)]
 for row in range(r):
     for col in range(c):
         twod_arr[row][col] = row*col
+        # after below line, it will go directly to twod_arr line and matrix will be printed then
 print(twod_arr)
