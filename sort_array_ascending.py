9c734bd8b720d6c2ef211b140001ef929a2c8c2a
Saur03 <chawlasaurabh97@gmail.com>
2023-07-12 19:23:27 +0530

    sort the array in ascending order

--- sort_array_ascending.py
+++ sort_array_ascending.py
@@ -1,9 +1,16 @@
+'''
+In this, we have sort the array in ascending order.
+
+'''
 arr = [10, 22, 38,27, 11];
 temp = 0;
 
+# Display elements of array 
 print("Elements of original array: ");
 for i in range(0, len(arr)):
     print(arr[i], end=" ");
+    
+#Sort the array in Ascending order
 for i in range(0, len(arr)):
     for j in range(i+1, len(arr)):
         if (arr[i] > arr[j]):
@@ -11,6 +18,8 @@ for i in range(0, len(arr)):
             arr[i] = arr[j];
             arr[j] = temp;
 print();
+
+# Display elements of the array in Ascending order
 print("Elements of array sorted in ascending order: ");
 for i in range(0, len(arr)):
     print(arr[i], end= " ");
