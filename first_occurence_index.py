29cbc6323a5dd2035ea5957ea502f44f2e7aeaa3
Saur03 <chawlasaurabh97@gmail.com>
2023-07-12 19:49:27 +0530

    first occurence of an element in an array and use import array class

--- first_occurence_index.py
+++ first_occurence_index.py
@@ -4,13 +4,14 @@ for example: Arr = [1,1,1]; Output for 1 will be '0'
 '''
 
 import array
-arr = array.array('u', ['c', 'a', 'b', 'd', 'e', 'f'])
-
+#Created an array
+arr = array.array('i', [1, 2, 3, 1, 2, 5])
 print ("The new created array is : ", end = " ")
 for i in range (0, 6):
     print (arr[i], end = " ")
 print ("\r")
-print (" The index of 1st occurrence of a is : ", end = "")
-print (arr.index('a'))
-print ("The index of 1st occurrence of f is :", end = "")
-print (arr.index('f'))
+print (" The index of 1st occurrence of 2 is : ", end = "")
+print (arr.index(2))
+# it will only count first occurence not the second tim,if the numbers are repeated.
+print ("The index of 1st occurrence of 1 is :", end = "")
+print (arr.index(1))
