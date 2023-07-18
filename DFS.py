1aea9f1ef89483e849d5e6cda901176295d55175
Saur03 <chawlasaurabh97@gmail.com>
2023-07-18 13:47:08 +0530

    DFS

--- DFS.py
+++ DFS.py
@@ -1,3 +1,4 @@
+# DFS is an algorithm to trace the nodes of the graph in a depth-first order; the nodes in one depth are traversed first, then the nodes in a paralled depth are traversed. It uses the concept of back-tracking sfter one vertical depth node is traversed.
 def dfs(graph, start, visited=None):
     if visited is None:
         visited = set()
