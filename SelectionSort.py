859965d5ab88e74bdab6de5d94c58f3b35ad533d
Saur03 <chawlasaurabh97@gmail.com>
2023-07-22 19:51:04 +0530

    Selection sort algo

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,3 +1,8 @@
+# Selection sort is a simple sort algorithm that revolves around the 'comparison'
+# In each iteration, one element gets placed.
+# We choose the minimum element in the array and place is at the beginning of the array by swapping with the front elemant
+# We can also do this by choosing maximum element and placing it at the rear end.
+# Selection sort basically selects an element in every iteration and place it at the appropriate position.
 def selectionSort(array, size):
     for step in range(size):
         min = step
